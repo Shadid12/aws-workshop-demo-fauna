56c80b63c23aaea484dd110d12bf299cfcc15e58
shadid12 <shadidhaque2012@gmail.com>
2022-07-13 11:52:52 -0400

    working fauna deployment

--- src/functions/bookFlight.js
+++ src/functions/bookFlight.js
@@ -1,7 +1,18 @@
 'use strict';
 
+// import faunadb driver
+const faunadb = require('faunadb')
+const q = faunadb.query;
+
+// initialize fauna client
+const serverClient = new faunadb.Client({ 
+  secret: process.env.FAUNA_ROOT_KEY,
+  domain: process.env.FAUNA_DOMAIN,
+});
+
 module.exports.handler = async (event, context) => {
   console.log('bookFlight', JSON.stringify(event, null, 2));
+
   if (event.flightId === -1) {
     throw new Error('Flight cannot be booked');
   }
--- src/functions/bookHotel.js
+++ src/functions/bookHotel.js
@@ -1,9 +1,52 @@
 'use strict';
 
+// import faunadb driver
+const faunadb = require('faunadb')
+const q = faunadb.query;
+
+// initialize fauna client
+const serverClient = new faunadb.Client({ 
+  secret: process.env.FAUNA_ROOT_KEY,
+  domain: process.env.FAUNA_DOMAIN,
+});
+
 module.exports.handler = async (event, context) => {
+  
   console.log('bookHotel', JSON.stringify(event, null, 2));
-  return {
-    statusCode: 200,
-    flightId: event.flightId
-  };
+  
+
+  try {
+    const newBooking = await serverClient.query(
+      q.Create(
+        q.Collection('Vacation'),
+        { 
+          data: {
+            userId: event.userId,
+            hotelBookingId: Math.random().toString(36).substring(2,12),
+            status: 'pending',
+          }
+        },
+      )
+    );
+
+    console.log('newBooking', JSON.stringify(newBooking));
+
+    // Fake a delay for external API call
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    // const updateBooking = await serverClient.query(
+    //   q.Update(
+    //     q.Ref(q.Collection('Vacation'), newBooking.id),
+    //     { data },
+    //   )
+    // )
+
+    return {
+      statusCode: 200,
+      newBooking,
+      flightId: event.flightId
+    };
+  } catch (error) {
+    throw new Error(error)
+  }
 };
